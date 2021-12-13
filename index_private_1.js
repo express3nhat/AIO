53db8d3b32c58e716cf8913e8e80d84079b4c3ed
express3nhat <95931825+express3nhat@users.noreply.github.com>
2021-12-13 19:57:59 +0700

    Update index_private_1.js

--- index_private_1.js
+++ index_private_1.js
@@ -66,7 +66,7 @@ window.addEventListener("orientationchange", function(event) {
 		QC.style.visibility = "hidden";
 		HD.style.visibility = "hidden";
 		
-		setTimeout(function(){ document.getElementById("myFrame").style.display = "block"; }, 2000);
+		setTimeout(function(){ document.getElementById("myFrame").style.display = "block"; }, 500);
 		
 		//document.getElementById("myFrame").style.transform = "rotate(0deg)";
 	}
