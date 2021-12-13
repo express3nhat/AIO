734e988c121ae2544633c6982e2a6a8195b53e72
express3nhat <95931825+express3nhat@users.noreply.github.com>
2021-12-13 19:39:54 +0700

    Update index_private_1.js

--- index_private_1.js
+++ index_private_1.js
@@ -1,7 +1,7 @@
 
 
 var exec = "https://script.google.com/macros/s/AKfycbyg0Nad76648wOF66A173iU1y3MRdt9VoOAYbtC/exec"; //EXPRESS3NHAT
-var ifr = document.getElementById("myFrame");
+
 //var w;
 
     window.addEventListener("load",function() {
@@ -25,7 +25,7 @@ var ifr = document.getElementById("myFrame");
 	   str = "?embedIframe";
 	}	
 
-	    
+	    var ifr = document.getElementById("myFrame");
 	    
 	    //ifr.style.width = w;
 	    
@@ -51,7 +51,7 @@ window.addEventListener("orientationchange", function(event) {
 		//alert("the orientation of the device is now " + event.target.screen.orientation.angle);
 		QC.style.visibility = "visible";
 		HD.style.visibility = "hidden";
-		ifr.style.display = "none";
+		//ifr.style.display = "none";
 		//document.getElementById("myFrame").style.transform = "rotate(90deg)";
 	} 
 	
@@ -59,7 +59,7 @@ window.addEventListener("orientationchange", function(event) {
 		//alert("the orientation of the device is now " + event.target.screen.orientation.angle);
 		HD.style.visibility = "visible";
 		QC.style.visibility = "hidden";
-		ifr.style.display = "none";
+		//ifr.style.display = "none";
 		//document.getElementById("myFrame").style.transform = "rotate(270deg)";
 	}
 	if (event.target.screen.orientation.angle==0) {
